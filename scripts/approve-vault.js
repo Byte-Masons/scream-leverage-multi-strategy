6abaa637c07434d5b24c346704dfeea0f6507b8f
Magnus Brantheim <m.brantheim@gmail.com>
2022-07-02 10:51:17 +0200

    Update scripts

--- scripts/approve-vault.js
+++ scripts/approve-vault.js
@@ -3,7 +3,7 @@ async function main() {
   const ERC20 = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
   const wantAddress = '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83';
   const want = await ERC20.attach(wantAddress);
-  await want.approve(vaultAddress, ethers.utils.parseEther('100'));
+  await want.approve(vaultAddress, ethers.utils.parseEther('1000'));
   console.log('want approved');
 }
 
