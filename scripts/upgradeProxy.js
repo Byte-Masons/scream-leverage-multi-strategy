e3e4542f8fc8e82450fac5fdc2a865c0b3b7304b
Magnus Brantheim <m.brantheim@gmail.com>
2022-07-07 13:44:43 +0200

    Upgrade wftm strategy

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -2,8 +2,6 @@ async function main() {
   const stratFactory = await ethers.getContractFactory('ReaperStrategyScreamLeverage');
   const stratContract = await hre.upgrades.upgradeProxy('0xd2e77d311dDca106d64c61E8CCb258d37636dd68', stratFactory, {
     timeout: 0,
-    gasPrice: 300000000000,
-    gasLimit: 9000000,
   });
   console.log('Strategy upgraded!');
 }
