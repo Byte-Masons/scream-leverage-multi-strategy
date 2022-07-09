77cc5252ff4829d2ed1c7a33d46cecfdc2e6d08f
Magnus Brantheim <m.brantheim@gmail.com>
2022-07-09 18:29:23 +0200

    Upgraded ETH strategy

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,7 +1,6 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyScreamLeverage');
+  const stratContract = await hre.upgrades.upgradeProxy('0xaaBFBC79DaaA5e9B882EE10D4acCB96c72e366A8', stratFactory, {
     timeout: 0,
   });
   console.log('Strategy upgraded!');
