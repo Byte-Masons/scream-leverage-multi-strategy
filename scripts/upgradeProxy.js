f82069564cb477d1acf539c7f0fbb46a65cdda8c
Magnus Brantheim <m.brantheim@gmail.com>
2022-07-09 18:43:10 +0200

    Upgraded DAI strategy with loss underflow check

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,7 +1,6 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyScreamLeverage');
+  const stratContract = await hre.upgrades.upgradeProxy('0x2fbEDa4876341Ef0Bcb4AA9e135Bb99e41A09CC4', stratFactory, {
     timeout: 0,
   });
   console.log('Strategy upgraded!');
